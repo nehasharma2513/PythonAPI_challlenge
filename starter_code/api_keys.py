ab6c5ac8177eacc1cbe47b63060864adb2b535e1
nehasharma2513 <142134132+nehasharma2513@users.noreply.github.com>
2023-10-02 14:47:08 -0400

    Update api_keys.py

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "b481ca5dd8aee321e68bc9fc76a7449d"
+weather_api_key = 
 
 # Geoapify API Key
-geoapify_key = "b22ef2ad3563422dadb2905f4ec19826"
+geoapify_key = 
